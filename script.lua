8270884211b477c95065ffd0e1c782946c04b017
strdragon <158556828+strdragon@users.noreply.github.com>
2024-12-10 12:15:23 +0700

    script.lua
    ledengs of dpeed

---
+++ script.lua
@@ -0,0 +1,270 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Turtle-Brand/Turtle-Lib/main/source.lua"))()
+
+local window = library:Window("Legends Of Speed script")
+
+window:Button("auto farm lvl", function()
+    while wait() do
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Orange Orb",
+            [3] = "City"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Blue Orb",
+            [3] = "City"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+        
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Yellow Orb",
+            [3] = "City"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Red Orb",
+            [3] = "City"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Orange Orb",
+            [3] = "Snow City"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+        
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Yellow Orb",
+            [3] = "Snow City"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Blue Orb",
+            [3] = "Snow City"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Red Orb",
+            [3] = "Snow City"
+        }
+
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Orange Orb",
+            [3] = "Magma City"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+        
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Yellow Orb",
+            [3] = "Magma City"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Blue Orb",
+            [3] = "Magma City"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Red Orb",
+            [3] = "Magma City"
+        }
+
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+        
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Red Orb",
+            [3] = "Speed Desert"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Blue Orb",
+            [3] = "Speed Desert"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Yellow Orb",
+            [3] = "Speed Desert"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Orange Orb",
+            [3] = "Speed Desert"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+        
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Red Orb",
+            [3] = "Outer Space"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Blue Orb",
+            [3] = "Outer Space"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Yellow Orb",
+            [3] = "Outer Space"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Orange Orb",
+            [3] = "Outer Space"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+    end
+ end)
+
+window:Button("auto farm gem", function()
+    while wait() do
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Gem",
+            [3] = "City"
+        }   
+
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Gem",
+            [3] = "Snow City"
+        }
+      
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))    
+        
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Gem",
+            [3] = "Magma City"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args)) 
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Gem",
+            [3] = "Speed Desert"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args)) 
+
+        local args = {
+            [1] = "collectOrb",
+            [2] = "Gem",
+            [3] = "Outer Space"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args)) 
+    end        
+ end)
+
+window:Button("auto rebitrh", function()
+    while wait() do
+        local args = {
+            [1] = "rebirthRequest"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("rebirthEvent"):FireServer(unpack(args))
+    end        
+ end)
+
+window:Button("collect all chest", function()
+
+    local args = {
+        [1] = "Golden Chest"
+    }
+    
+    game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("checkChestRemote"):InvokeServer(unpack(args))
+
+    local args = {
+        [1] = workspace:WaitForChild("rewardChests"):WaitForChild("rewardChest")
+    }
+    
+    game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("collectCourseChestRemote"):InvokeServer(unpack(args))
+
+    local args = {
+        [1] = "groupRewards"
+    }
+    
+    game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("groupRemote"):InvokeServer(unpack(args))
+    
+    local args = {
+        [1] = "Enchanted Chest"
+    }
+    
+    game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("checkChestRemote"):InvokeServer(unpack(args))
+
+    local args = {
+        [1] = "Magma Chest"
+    }
+    
+    game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("checkChestRemote"):InvokeServer(unpack(args))
+
+ end)
+
+window:Button("auto join to the race", function()
+    while wait() do
+
+    local args = {
+        [1] = "joinRace"
+    }
+    
+    game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("raceEvent"):FireServer(unpack(args))
+
+end
+
+end)
+
+window:Label("by dragonstrukov", Color3.fromRGB(127, 143, 166))
